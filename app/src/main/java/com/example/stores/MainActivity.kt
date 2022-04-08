37ba27d5a8c6d7ad74f33bb811a2b31c820115c1
David Roberto Acosta Saucedo <David.Sauced@Outlook.com>
2022-04-08 17:16:09 -0500

    Funciones de checkbox, agregar, eliminar implementadas.

--- app/src/main/java/com/example/stores/MainActivity.kt
+++ app/src/main/java/com/example/stores/MainActivity.kt
@@ -2,10 +2,81 @@ package com.example.stores
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.stores.databinding.ActivityMainBinding
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
+
+    private lateinit var mbinding: ActivityMainBinding
+    private lateinit var mAdapter: StoreAdapter
+    private lateinit var mGridLayout: GridLayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mbinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mbinding.root)
+
+        mbinding.btnSave.setOnClickListener {
+            val store = StoreEntity(name = mbinding.etName.text.toString().trim())
+
+            Thread {
+                StoreApplication.database.storeDao().addStore(store)
+            }.start()
+
+            mAdapter.add(store)
+        }
+
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        mAdapter = StoreAdapter(mutableListOf(), this)
+        mGridLayout = GridLayoutManager(this, 2)
+        getStores()
+
+        mbinding.recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = mGridLayout
+            adapter = mAdapter
+        }
+    }
+
+
+    private fun getStores() {
+        doAsync {
+            val stores = StoreApplication.database.storeDao().getAllStores()
+            uiThread {
+                mAdapter.setStores(stores)
+            }
+        }
+    }
+
+
+    /*
+    *OnClickListener
+     */
+    override fun onClick(storeEntity: StoreEntity) {
+
+    }
+
+    override fun onFavoriteStore(storeEntity: StoreEntity) {
+        storeEntity.isFavorite = !storeEntity.isFavorite
+        doAsync {
+            StoreApplication.database.storeDao().updateStore(storeEntity)
+            uiThread {
+                mAdapter.update(storeEntity)
+            }
+        }
+    }
+
+    override fun onDeleteStore(storeEntity: StoreEntity) {
+        doAsync {
+            StoreApplication.database.storeDao().deleteStore(storeEntity)
+            uiThread {
+                mAdapter.delete(storeEntity)
+            }
+        }
     }
 }
