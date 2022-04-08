8732a84d38098f9c48d5f61b7fd32f1adff10cec
David Roberto Acosta Saucedo <David.Sauced@Outlook.com>
2022-04-08 15:13:32 -0500

    Listener y Adapter creados.

---
+++ app/src/main/java/com/example/stores/StoreAdapter.kt
@@ -0,0 +1,12 @@
+package com.example.stores
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.stores.databinding.ItemStoreBinding
+
+class StoreAdapter {
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        val binding = ItemStoreBinding.bind(view)
+    }
+}
